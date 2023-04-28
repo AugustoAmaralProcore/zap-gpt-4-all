415e7efd3761752b51844265cbae50c51b35cfb9
Augusto Amaral <augusto.pereira-contractor@procore.com>
2023-04-28 12:27:39 -0300

    First test

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,61 @@
 import { GPT4All } from 'gpt4all';
+import { Client, LocalAuth } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
 
-const main = async () => {
-  // Instantiate GPT4All with default or custom settings
-  const gpt4all = new GPT4All('gpt4all-lora-quantized', true); // Default is 'gpt4all-lora-quantized' model
+const zapClient = new Client({
+  authStrategy: new LocalAuth({
+    clientId: 'browser_client',
+  }),
+  puppeteer: {
+    headless: true,
+    timeout: 90000,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu',
+    ],
+  },
+});
 
-  // Initialize and download missing files
+const gpt4all = new GPT4All('gpt4all-lora-quantized', true);
+
+zapClient.initialize();
+
+zapClient.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+zapClient.on('ready', () => {
+  console.log('GPT Zap iniciado com sucesso!');
+});
+
+function removeAnsiEscapeCodes(str: string) {
+  return str.replace(/\x1b\[[0-9;]*[a-zA-Z]/g, '');
+}
+zapClient.on('message', async (message) => {
+  const isGroup = (await message.getChat()).isGroup;
+  if (isGroup === true) {
+    return;
+  }
+
   await gpt4all.init();
 
   // Open the connection with the model
   await gpt4all.open();
-  // Generate a response using a prompt
-  const prompt =
-    'Tell me about how Open Access to AI is going to help humanity.';
-  const response = await gpt4all.prompt(prompt);
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Response: ${response}`);
 
-  const prompt2 =
-    'Explain to a five year old why AI is nothing to be afraid of.';
-  const response2 = await gpt4all.prompt(prompt2);
-  console.log(`Prompt: ${prompt2}`);
-  console.log(`Response: ${response2}`);
+  console.log(
+    `${new Date()} - ${message.from}: ${message.body.slice(0, 40) + '...'}`
+  );
+  const response = await gpt4all.prompt(
+    `Message from ${message.from}: ${message.body}`
+  );
+  console.log(`${new Date()} - ${message.from}-R: ${response}`);
+  await zapClient.sendMessage(message.from, removeAnsiEscapeCodes(response));
 
-  // Close the connection when you're done
   gpt4all.close();
-};
-
-main().catch(console.error);
+});
